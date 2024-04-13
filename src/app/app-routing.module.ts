5f13a2601526c148e429f632743ae0a2f24d6c70
onurku <onurku@gmail.com>
2024-04-13 12:29:48 +0300

    test

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+
+
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes} from '@angular/router';
+
+import { AppComponent } from './app.component';
+
+
+const routes: Routes = [
+  { path: '', component: AppComponent,  title: 'Anasayfa' },
+];
+
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+  providers: []
+})
+export class AppRoutingModule { }
