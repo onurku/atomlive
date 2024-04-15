1fbe1acbeb8241de057780375e2eeb0a0316e8f0
onurku <onurku@gmail.com>
2024-04-15 17:26:12 +0300

    check2

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'maintanance'`, () => {
+  it(`should have as title 'atombooks'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('maintanance');
+    expect(app.title).toEqual('atombooks');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('maintanance app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('atombooks app is running!');
   });
 });
